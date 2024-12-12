ba2a8f5c9a976bf0dfffb584b4e267a876f9b82a
Lucas Grillos <lucas.em.grillos@gmail.com>
2024-12-11 23:02:18 -0500

    add basic movement

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ canvas.width = 600;
 canvas.height = 600;
 const ctx = canvas.getContext("2d");
 
+SHIP_VEL = 1;
+SHIP_DIAG_VEL = 0.75;
+
+const MOVE_KEYS = {
+    w: false,
+    a: false,
+    s: false, 
+    d: false
+}
+
 class Player {
     constructor() {
         this.hitbox = {
             height: 40,
             width: 30
+        }
+
+        this.velocity = {
+            x: 0,
+            y: 0
         }
 
         this.position = {
@@ -24,6 +39,38 @@ class Player {
             width: 10,
             height: 40
         }
+    }
+
+    setVelocity() {
+        if (MOVE_KEYS["w"]===true && MOVE_KEYS["s"]!== true) {
+            this.velocity.y = -SHIP_VEL;
+        } 
+        else if (MOVE_KEYS["w"]!==true && MOVE_KEYS["s"]!==true) {
+            this.velocity.y = 0;
+        } 
+        
+        if (MOVE_KEYS["a"]===true && MOVE_KEYS["d"]!==true) {
+            this.velocity.x = -SHIP_VEL;
+        } else  if(MOVE_KEYS["a"]!==true && MOVE_KEYS["d"]!==true) {
+            this.velocity.x = 0;
+        }
+        
+        if (MOVE_KEYS["s"]===true && MOVE_KEYS["w"]!==true) {
+            this.velocity.y = SHIP_VEL;
+        } else if (MOVE_KEYS["s"]!==true && MOVE_KEYS["w"]!== true){
+            this.velocity.y = 0;
+        } 
+        
+        if (MOVE_KEYS["d"]===true && MOVE_KEYS["a"]!==true) {
+            this.velocity.x = 1;
+        } else if(MOVE_KEYS["d"]!==true && MOVE_KEYS["a"]!==true) {
+            this.velocity.x = 0;
+        }
+    }
+
+    move() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y
     }
 
     draw() {
@@ -46,9 +93,31 @@ const animate = () => {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     player.draw()
-    
+    player.setVelocity()
+    player.move();
 }
 
 const player = new Player();
 
+const keyDown = (event) => {
+    //console.log(event.key);
+    if (event.key in MOVE_KEYS) {
+        //console.log(true);
+        velocityOnOff(event.key, true);
+    }
+}
+
+const keyUp = (event) => {
+    if (event.key in MOVE_KEYS) {
+        velocityOnOff(event.key, false);
+    }
+}
+
+const velocityOnOff = (key, bool) => {
+    MOVE_KEYS[key] = bool;
+    //console.log(MOVE_KEYS[key])
+}
+
 window.onload = animate();
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
